ea70dacc3b15996f5d9ef4da6713c29f6911935b
Matteo Marchegiani <matmarcheg@gmail.com>
2024-08-18 14:58:22 +0200

    Sequential layers and hparams

--- models/binary_classifier.py
+++ models/binary_classifier.py
@@ -4,8 +4,9 @@ import torch.nn as nn
 class BinaryClassifier(pl.LightningModule):
     def __init__(self, input_size, hidden_size, output_size, num_hidden_layers, learning_rate=1e-3):
         super(BinaryClassifier, self).__init__()
+        self.save_hyperparameters() # Save hyperparameters in self.hparams
         self.learning_rate = learning_rate
-        self.criterion = nn.BCEWithLogitsLoss(reduction='none')
+        self.criterion = nn.BCEWithLogitsLoss(reduction='none') # Note: with the BCEWithLogitsLoss, a sigmoid is applied to the output of the model
 
         layers = [
             nn.BatchNorm1d(input_size, affine=False), # Batch normalization layer
@@ -20,11 +21,10 @@ class BinaryClassifier(pl.LightningModule):
         layers.append(nn.ReLU())
         layers.append(nn.Linear(hidden_size, output_size)) # Output layer
 
-        self.layers = nn.ModuleList(layers)
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
+        x = self.layers(x)
         
         return x
 
@@ -46,6 +46,12 @@ class BinaryClassifier(pl.LightningModule):
         #self.log('test_acc', acc, prog_bar=True)
         #return {'loss': loss, 'acc': acc}
         return {'loss': loss}
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=None):
+        x, y, w = batch
+        y_pred = self(x)
+        y_pred = torch.sigmoid(y_pred)
+        return y_pred
 
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), lr=self.learning_rate)
