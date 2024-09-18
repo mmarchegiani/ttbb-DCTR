08c06e8e670ce806559507ab1c48d09a7259ddbd
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-18 11:30:31 +0200

    Script for trainings at different LR

---
+++ scripts/submit_lr_trainings.sh
@@ -0,0 +1,3 @@
+python scripts/submit_to_condor.py --cfg parameters/run_parameters.yaml  --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/binary_classifier_26features_full_Run2_batch8092_lr1e-3 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4.yaml  --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/binary_classifier_26features_full_Run2_batch8092_lr5e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3.yaml  --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/binary_classifier_26features_full_Run2_batch8092_lr2e-3 --good-gpus
