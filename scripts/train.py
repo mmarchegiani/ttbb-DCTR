54e9384b5b41d6acabc50673ffbbc686a5808f16
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-19 01:47:04 +0200

    Update training script

--- scripts/train.py
+++ scripts/train.py
@@ -12,26 +12,27 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 from ttbb_dctr.lib.data_preprocessing import get_dataloaders
 from ttbb_dctr.models.binary_classifier import BinaryClassifier
+from ttbb_dctr.utils.utils import get_device
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, help="Config file with parameters for data preprocessing and training", required=True)
     parser.add_argument('-l', '--log_dir', type=str, help="Output folder", required=True)
-    parser.add_argument('--save', action='store_true', help="Save the train and test datasets")
     args = parser.parse_args()
 
     cfg = OmegaConf.load(args.cfg)
 
     cfg_model = cfg["model"]
+    cfg_training = cfg["training"]
 
     os.makedirs(args.log_dir, exist_ok=True)
-    train_dataloader, val_dataloader = get_dataloaders(cfg)
+    train_dataloader, val_dataloader, input_size = get_dataloaders(cfg, input_size=True)
 
     # Instantiate the model
-    input_size = len(input_features)
-    model = BinaryClassifier(input_size, **cfg_model, learning_rate=cfg_training["learning_rate"])
+    model = BinaryClassifier(input_size, **cfg_model, learning_rate=cfg_training["learning_rate"], weight_decay=cfg_training["weight_decay"])
 
     # Move the model to device and set training mode
+    device = get_device()
     model = model.to(device)
     print("Initialized model:")
     print(model)
@@ -39,7 +40,7 @@ if __name__ == "__main__":
     callbacks = [
         ModelCheckpoint(monitor="val_loss", mode="min", save_top_k=-1, filename="{epoch:03d}-{step:03d}-{val_loss:.2f}", save_last=True, verbose=True),
         EarlyStopping(monitor="val_loss", min_delta=0.00, patience=cfg_training["patience"], verbose=False, mode="min"),
-        LearningRateMonitor(logging_interval='epoch')
+        LearningRateMonitor(logging_interval='epoch', log_weight_decay=True)
     ]
     trainer = Trainer(max_epochs=cfg_training["epochs"], default_root_dir=args.log_dir, callbacks=callbacks)
     print("Training model...")
