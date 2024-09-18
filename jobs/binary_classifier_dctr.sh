108026b823619969bc9d65c07eabc9b41a27ccfb
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-18 11:31:34 +0200

    Job submission script

---
+++ jobs/binary_classifier_dctr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TTHBB_SPANET_DIR=/afs/cern.ch/work/m/mmarcheg/ttHbb/ttHbb_SPANet
+TTBB_DCTR_DIR=/afs/cern.ch/work/m/mmarcheg/ttHbb/ttbb-DCTR
+NUM_GPU=1
+
+# Create venv in local job dir
+python -m venv myenv --system-site-packages
+source myenv/bin/activate
+
+# Install ttHbb_SPANet in virtual environment
+cd $TTHBB_SPANET_DIR
+pip install -e .
+cd -
+
+cd $TTBB_DCTR_DIR
+pip install -e .
+cd -
+
+# Launch training
+python $TTBB_DCTR_DIR/scripts/train.py --cfg $1 --log_dir $2
