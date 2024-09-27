9f8c8795f526e393376873f06a808969c3979fb3
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-27 15:27:46 +0200

    Trainings with 8 input features

--- scripts/submit_8features_trainings.sh
+++ scripts/submit_8features_trainings.sh
@@ -1,3 +1,4 @@
-python scripts/submit_to_condor.py --cfg parameters/run_parameters.yaml  --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/binary_classifier_26features_full_Run2_batch8092_lr1e-3 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4.yaml  --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/binary_classifier_26features_full_Run2_batch8092_lr5e-4 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3.yaml  --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/binary_classifier_26features_full_Run2_batch8092_lr2e-3 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/features_8/run_parameters_lr1e-1.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/features_8/binary_classifier_8features_full_Run2_batch8092_lr1e-1 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/features_8/run_parameters_lr1e-2.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/features_8/binary_classifier_8features_full_Run2_batch8092_lr1e-2 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/features_8/run_parameters_lr1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/features_8/binary_classifier_8features_full_Run2_batch8092_lr1e-3 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/features_8/run_parameters_lr1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/features_8/binary_classifier_8features_full_Run2_batch8092_lr1e-4 --good-gpus
