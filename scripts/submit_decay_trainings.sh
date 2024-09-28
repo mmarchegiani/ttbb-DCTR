fa45380f40829f267e056a40457d580bad31df7c
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-28 18:28:36 +0200

    Submit trainings with njet reweighting saved

--- scripts/submit_decay_trainings.sh
+++ scripts/submit_decay_trainings.sh
@@ -1,12 +1,12 @@
 # lr = 1e-3
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr1e-3_decay1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr1e-3_decay1e-3 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr1e-3_decay1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr1e-3_decay1e-4 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr1e-3_decay5e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr1e-3_decay5e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr1e-3_decay1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr1e-3_decay1e-3 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr1e-3_decay1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr1e-3_decay1e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr1e-3_decay5e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr1e-3_decay5e-4 --good-gpus
 # lr = 2e-3
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3_decay1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr2e-3_decay1e-3 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3_decay1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr2e-3_decay1e-4 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3_decay5e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr2e-3_decay5e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3_decay1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr2e-3_decay1e-3 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3_decay1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr2e-3_decay1e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr2e-3_decay5e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr2e-3_decay5e-4 --good-gpus
 # lr = 5e-4
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4_decay1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr5e-4_decay1e-3 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4_decay1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr5e-4_decay1e-4 --good-gpus
-python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4_decay5e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet/binary_classifier_26features_full_Run2_batch8092_lr5e-4_decay5e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4_decay1e-3.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr5e-4_decay1e-3 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4_decay1e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr5e-4_decay1e-4 --good-gpus
+python scripts/submit_to_condor.py --cfg parameters/run_parameters_lr5e-4_decay5e-4.yaml --log_dir /eos/user/m/mmarcheg/ttHbb/dctr/training/reweigh_njet_v2/binary_classifier_26features_full_Run2_batch8092_lr5e-4_decay5e-4 --good-gpus
