1b93200003ab180f03356b82f6eccb4b15daaf58
Matteo Marchegiani <matmarcheg@gmail.com>
2024-11-13 22:34:29 +0100

    Apply tt+LF reweighting before computing 1D reweighting

--- scripts/dataset/parquet_to_dctr.py
+++ scripts/dataset/parquet_to_dctr.py
@@ -30,27 +30,32 @@ if __name__ == "__main__":
     datasets = get_datasets_list(cfg_input)
     dataset = DCTRDataset(datasets, cfg_preprocessing, shuffle=True, reweigh=True, has_data=True)
     dataset.store_masks({k : get_cr_mask(dataset.df, v) for k, v in cfg_cuts.items()})
-    # Apply a pre-computed njet based reweighting if specified in the configuration file
-    # This is needed to apply the same reweighting as applied to the training dataset, to the test dataset
     if cfg["preprocessing"].get("weights", None) is not None:
         cfg_weights = cfg["preprocessing"]["weights"]
     else:
         cfg_weights = {}
-    # Store the njet reweighting factor
-    if cfg_weights.get("njet_reweighting", None) is not None:
-        cfg_njet_reweighting = cfg_weights["njet_reweighting"]
-        with open(cfg_njet_reweighting["file"]) as f:
-            njet_reweighting_map = yaml.load(f, Loader=yaml.FullLoader)[cfg_njet_reweighting["key"]]
-        dataset.apply_weight(dataset.df, get_njet_reweighting(dataset.df, njet_reweighting_map))
-    if not args.dry:
-        if args.compute_njet_reweighting:
-            if cfg_weights.get("njet_reweighting", None) is not None:
-                raise ValueError("njet reweighting already computed and saved in the configuration file. Please remove the --compute-njet-reweighting flag.")
-            # Apply the tt+LF reweighting before computing the njet reweighting
-            if cfg_weights.get("ttlf_reweighting", None) is not None:
-                cfg_ttlf_reweighting = cfg_weights["ttlf_reweighting"]
-                dataset.apply_weight(dataset.df, get_ttlf_reweighting(dataset.df, cfg_ttlf_reweighting))
-            dataset.compute_njet_weights()
-            dataset.save_reweighting_map(args.output.replace(".parquet", "_reweighting_map.yaml"))
-        else:
+    if args.compute_njet_reweighting:
+        if cfg_weights.get("njet_reweighting", None) is not None:
+            raise ValueError("njet reweighting already computed and saved in the configuration file. Please remove the --compute-njet-reweighting flag.")
+        # Apply the tt+LF reweighting before computing the njet reweighting
+        if cfg_weights.get("ttlf_reweighting", None) is not None:
+            cfg_ttlf_reweighting = cfg_weights["ttlf_reweighting"]
+            dataset.apply_weight(dataset.df, get_ttlf_reweighting(dataset.df, cfg_ttlf_reweighting))
+        dataset.compute_njet_weights()
+        dataset.save_reweighting_map(args.output.replace(".parquet", "_reweighting_map.yaml"))
+    else:
+        # Apply a pre-computed njet based reweighting if specified in the configuration file
+        # This is needed to apply the same reweighting as applied to the training dataset, to the test dataset
+        # Apply it only to the events that need to be reweighted by the DCTR (i.e. ttbb events)
+        if cfg_weights.get("njet_reweighting", None) is not None:
+            cfg_njet_reweighting = cfg_weights["njet_reweighting"]
+            with open(cfg_njet_reweighting["file"]) as f:
+                njet_reweighting_map = yaml.load(f, Loader=yaml.FullLoader)[cfg_njet_reweighting["key"]]
+            mask_ttbb = dataset.df.dctr == 0
+            dataset.apply_weight(dataset.df, get_njet_reweighting(dataset.df, njet_reweighting_map, mask=mask_ttbb))
+        # Apply the tt+LF reweighting if specified in the configuration file and update the weights before saving the output
+        if cfg_weights.get("ttlf_reweighting", None) is not None:
+            cfg_ttlf_reweighting = cfg_weights["ttlf_reweighting"]
+            dataset.apply_weight(dataset.df, get_ttlf_reweighting(dataset.df, cfg_ttlf_reweighting))
+        if not args.dry:
             dataset.save_all(args.output)
