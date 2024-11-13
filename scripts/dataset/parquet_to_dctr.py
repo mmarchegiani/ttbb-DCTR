b6586a937803f2ac469ed15baf732a2421d3b1f6
Matteo Marchegiani <matmarcheg@gmail.com>
2024-11-13 14:32:26 +0100

    Include tt+LF correction before computing 1D reweweighting

--- scripts/dataset/parquet_to_dctr.py
+++ scripts/dataset/parquet_to_dctr.py
@@ -10,15 +10,14 @@ import awkward as ak
 from omegaconf import OmegaConf
 
 from tthbb_spanet import DCTRDataset
-from ttbb_dctr.lib.data_preprocessing import get_datasets_list, get_cr_mask, get_njet_reweighting
+from ttbb_dctr.lib.data_preprocessing import get_datasets_list, get_cr_mask, get_njet_reweighting, get_ttlf_reweighting
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, help="Config file with parameters for data preprocessing and training", required=True)
     parser.add_argument('-o', '--output', type=str, help="Output file path", required=True)
-    parser.add_argument('--reweigh-njet', action='store_true', help="Apply 1D reweighting based on the number of jets")
+    parser.add_argument('--compute-njet-reweighting', action='store_true', help="Apply 1D reweighting based on the number of jets")
     parser.add_argument('--dry', action='store_true', help="Dry run, do not save the dataset")
-    parser.add_argument('--cr2', action='store_true', help="Apply reweighting for CR2")
     args = parser.parse_args()
 
     cfg = OmegaConf.load(args.cfg)
@@ -31,15 +30,27 @@ if __name__ == "__main__":
     datasets = get_datasets_list(cfg_input)
     dataset = DCTRDataset(datasets, cfg_preprocessing, shuffle=True, reweigh=True, has_data=True)
     dataset.store_masks({k : get_cr_mask(dataset.df, v) for k, v in cfg_cuts.items()})
-    if args.cr2:
-        assert len(list(cfg_cuts.keys())) == 1
-        key = list(cfg_cuts.keys())[0]
-        with open("/eos/user/m/mmarcheg/ttHbb/dctr/training_datasets/full_Run2_26features_v3/dataset_dctr_26features_reweighting_map.yaml") as f:
-            njet_reweighting_map = yaml.load(f, Loader=yaml.FullLoader)["tthbb0p40_ttlf0p60"]
-        dataset.store_weight(key, get_njet_reweighting(dataset.df, njet_reweighting_map, dataset.masks[key]))
-    if args.reweigh_njet:
-        dataset.compute_njet_weights()
+    # Apply a pre-computed njet based reweighting if specified in the configuration file
+    # This is needed to apply the same reweighting as applied to the training dataset, to the test dataset
+    if cfg["preprocessing"].get("weights", None) is not None:
+        cfg_weights = cfg["preprocessing"]["weights"]
+    else:
+        cfg_weights = {}
+    # Store the njet reweighting factor
+    if cfg_weights.get("njet_reweighting", None) is not None:
+        cfg_njet_reweighting = cfg_weights["njet_reweighting"]
+        with open(cfg_njet_reweighting["file"]) as f:
+            njet_reweighting_map = yaml.load(f, Loader=yaml.FullLoader)[cfg_njet_reweighting["key"]]
+        dataset.apply_weight(dataset.df, get_njet_reweighting(dataset.df, njet_reweighting_map))
     if not args.dry:
-        dataset.save_all(args.output)
-        if args.reweigh_njet:
+        if args.compute_njet_reweighting:
+            if cfg_weights.get("njet_reweighting", None) is not None:
+                raise ValueError("njet reweighting already computed and saved in the configuration file. Please remove the --compute-njet-reweighting flag.")
+            # Apply the tt+LF reweighting before computing the njet reweighting
+            if cfg_weights.get("ttlf_reweighting", None) is not None:
+                cfg_ttlf_reweighting = cfg_weights["ttlf_reweighting"]
+                dataset.apply_weight(dataset.df, get_ttlf_reweighting(dataset.df, cfg_ttlf_reweighting))
+            dataset.compute_njet_weights()
             dataset.save_reweighting_map(args.output.replace(".parquet", "_reweighting_map.yaml"))
+        else:
+            dataset.save_all(args.output)
